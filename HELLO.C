aab9b97ebf6ebd9351df2024b7633b4fd47f51fc
rajkumar19 <31241801+rajkumar19@users.noreply.github.com>
2017-08-29 16:10:50 +0530

    Create HELLO.C

---
+++ HELLO.C
@@ -0,0 +1,11 @@
+/*
+* C Program to print "Hello World" 5 times using for loop
+*/
+#include <stdio.h>
+void main()
+{
+    int counter;
+    for(counter = 0; counter < 5; counter++);
+    printf("Hello World\n");
+ }
+   
